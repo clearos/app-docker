7b3f8ed2b36fbcb9e4b692ab3b8dad22d16b84cf
pcbaldwin <peter@egloo.ca>
2018-03-06 00:08:07 -0500

    Bumped version

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'docker';
-$app['version'] = '2.5.5';
+$app['version'] = '2.5.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-docker.spec
+++ packaging/app-docker.spec
@@ -1,7 +1,7 @@
 
 Name: app-docker
 Epoch: 1
-Version: 2.5.5
+Version: 2.5.6
 Release: 1%{dist}
 Summary: Docker
 License: GPLv3
